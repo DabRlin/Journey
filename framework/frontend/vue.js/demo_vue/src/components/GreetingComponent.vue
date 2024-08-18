1c0c625db8a7d00665283624cb52423eda6a9e4d
DabRlin <dang.hongyuanchen@gmail.com>
2024-08-19 06:06:32 +0800

    vue data&command review & event test

---
+++ framework/frontend/vue.js/demo_vue/src/components/GreetingComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Hello,{{ name }}!</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Alice",
+    };
+  },
+};
+</script>
+
+<style scoped>
+p{
+    color: ;
+}
+</style>
